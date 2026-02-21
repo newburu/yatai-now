dd8e0ae854f0ac6d3b15171c45a9d4e1ecba1acf
Newburu <newburu@gmail.com>
2026-02-21 22:07:30 +0900

    Change database from PostgreSQL to MariaDB

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2025_11_30_053250) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
