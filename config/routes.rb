4e212c98e1f6ec43e5066ce1342bffb66be2832d
Newburu <newburu@gmail.com>
2026-02-23 22:17:28 +0900

    feat: 管理者画面でユーザーのパスワード変更を可能にし、Deviseの新規ユーザー登録を無効化

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,12 @@ Rails.application.routes.draw do
 
     get "sender", to: "sender#index"
 
-    devise_for :users
+    devise_for :users, skip: [ :registrations ]
+    as :user do
+      get "users/edit" => "devise/registrations#edit", as: "edit_user_registration"
+      put "users" => "devise/registrations#update", as: "user_registration"
+      patch "users" => "devise/registrations#update"
+    end
 
     namespace :admin do
       resource :settings, only: [ :edit, :update ]
